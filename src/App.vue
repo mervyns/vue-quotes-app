81ab42a51019c5868c247dd292c661b2fe5b6313
mervyns <mervyns@gmail.com>
2018-12-28 14:09:19 +0800

    added quote grid view and add quote function

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,32 @@
 <template>
     <div class="container">
-    
+    <app-new-quote @addQuote="newQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+
     export default {
-        
+        data: () => {
+            return {
+                quotes: [
+                    'A simple quote'
+                ],
+                maxQuotes: 10
+            }
+        },
+        methods: {
+            newQuote(quote) {
+                this.quotes.push(quote)
+            }
+        },
+        components: {
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
+        }
     }
 </script>
 
